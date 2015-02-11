29b4e35f1d0675d0890f4714f7e2b2728ff73673
ianbacus <ianbacus@users.noreply.github.com>
2015-02-11 17:19:47 -0500

    Update barista.cpp

--- barista.cpp
+++ barista.cpp
@@ -15,31 +15,6 @@ using std::vector;
 
 //Begin OrangeJuiceOrderList Class Implementation
 
-//Destructor
-OrangeJuiceOrderList::~OrangeJuiceOrderList()
-{
-    for (unsigned int i=0; i<m_Order_List.size(); i++) {
-        delete m_Order_List[i];
-    }
-    m_Order_List.clear();
-}
-
-//Copy Constructor
-OrangeJuiceOrderList::OrangeJuiceOrderList (const OrangeJuiceOrderList & rhs)
-{
-    OrderList(rhs);
-}
-
-//Assignment Operator
-OrangeJuiceOrderList & OrangeJuiceOrderList::operator= (const OrangeJuiceOrderList &rhs)
-{
-    if (this != rhs)
-    {
-        m_Order_List = rhs.m_Order_List;
-    }
-    return *this;
-}
-
 
 
 //Begin BubbleTeaList Class Implementation
