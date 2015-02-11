48b7f2aa92ff2b66837480744dcae697d807035e
gtatyous93 <gtatyous@gmail.com>
2015-02-11 17:45:19 -0500

    Update orderlist.cpp

--- orderlist.cpp
+++ orderlist.cpp
@@ -1,52 +1,76 @@
-//Begin BubbleTeaList Class Implementation
+/*
+ * Team: Ian Bacus and Yousef Gtat
+ * CSE335
+ * Project01
+ * Date: 02/16/2015
+ *
+ *
+ *
+ */
 
-//Destructor
-BubbleTeaList::~BubbleTeaList()
-{
-    for (unsigned int i=0; i<m_Order_List.size(); i++) {
-        delete m_Order_List[i];
-    }
-    m_Order_List.clear();
-}
+#include "OrderList.h"
 
-//Copy Constructor
-BubbleTeaList::BubbleTeaList (const BubbleTeaList & rhs)
-{
-    OrderList(rhs);
-}
 
-//Assignment Operator
-BubbleTeaList & BubbleTeaList::operator= (const BubbleTeaList &rhs)
-{
-    if (this != rhs)
-    {
-        m_Order_List = rhs.m_Order_List;
-    }
-    return *this;
-}
-
+//Begin OrangeJuiceOrderList Class Implementation
 
 //Destructor
 OrangeJuiceOrderList::~OrangeJuiceOrderList()
 {
-    for (unsigned int i=0; i<m_Order_List.size(); i++) {
-        delete m_Order_List[i];
+    for (unsigned int i=0; i<m_OrderList_vector.size(); i++)
+    {
+        delete m_OrderList_vector[i];
     }
-    m_Order_List.clear();
+    m_OrderList_vector.clear();
 }
+
 
 //Copy Constructor
 OrangeJuiceOrderList::OrangeJuiceOrderList (const OrangeJuiceOrderList & rhs)
 {
-    OrderList(rhs);
+    m_OrderList_vector = rhs.m_OrderList_vector;
 }
+
 
 //Assignment Operator
 OrangeJuiceOrderList & OrangeJuiceOrderList::operator= (const OrangeJuiceOrderList &rhs)
 {
     if (this != rhs)
     {
-        m_Order_List = rhs.m_Order_List;
+        m_OrderList_vector = rhs.m_OrderList_vector;
+    }
+    return *this;
+}
+
+
+
+
+
+//Begin BubbleTeaList Class Implementation
+
+//Destructor
+BubbleTeaList::~BubbleTeaList()
+{
+    for (unsigned int i=0; i<m_OrderList_vector.size(); i++)
+    {
+        delete m_OrderList_vector[i];
+    }
+    m_OrderList_vector.clear();
+}
+
+
+//Copy Constructor
+BubbleTeaList::BubbleTeaList (const BubbleTeaList & rhs)
+{
+    m_OrderList_vector = rhs.m_OrderList_vector;
+}
+
+
+//Assignment Operator
+BubbleTeaList & BubbleTeaList::operator= /(const BubbleTeaList &rhs)
+{
+    if (this != rhs)
+    {
+        m_OrderList_vector = rhs.m_OrderList_vector;
     }
     return *this;
 }
