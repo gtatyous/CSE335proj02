57e5faab1c26db8c72d9b2f42ba4fcb54ef70e0f
ianbacus <ianbacus@users.noreply.github.com>
2015-02-11 17:22:38 -0500

    Update barista.cpp

--- barista.cpp
+++ barista.cpp
@@ -17,32 +17,6 @@ using std::vector;
 
 
 
-//Begin BubbleTeaList Class Implementation
-
-//Destructor
-BubbleTeaList::~BubbleTeaList()
-{
-    for (unsigned int i=0; i<m_Order_List.size(); i++) {
-        delete m_Order_List[i];
-    }
-    m_Order_List.clear();
-}
-
-//Copy Constructor
-BubbleTeaList::BubbleTeaList (const BubbleTeaList & rhs)
-{
-    OrderList(rhs);
-}
-
-//Assignment Operator
-BubbleTeaList & BubbleTeaList::operator= (const BubbleTeaList &rhs)
-{
-    if (this != rhs)
-    {
-        m_Order_List = rhs.m_Order_List;
-    }
-    return *this;
-}
 
 
 
